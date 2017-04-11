cfc3dfddf2ca8e3615efd0d70ddaf16a1e429b46
kaby76 <kenneth.domino@domemtech.com>
2017-04-10 22:03:05 -0400

    Adding in changes for example on Net Standard/Core/Framework interop.

---
+++ NetCore1AppSubstantial/NetCore1App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NetCore1App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = new NetFramework461LibSubstantial.Class1();
+            System.Console.WriteLine("Hello World from NetCore1App!");
+            var r = f.Fun1();
+            System.Console.WriteLine("list = " + r);
+        }
+    }
+}
---
+++ NetCore1AppSubstantialFixed/NetFramework461App/Program2.cs
@@ -0,0 +1,13 @@
+﻿namespace NetFramework461App
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            var f = new NetStandard14Lib.Class1();
+            var g = new NetFramework461LibSubstantial.Class1();
+            //var h = new NetCore1Lib.Class1();
+            System.Console.WriteLine("Hello World from NetFramework461App!");
+        }
+    }
+}
